4943b87a7c44a1f33442eebeb580d70bc183fa90
Rodrigo Finkler <finklerrg@gmail.com>
2022-10-06 00:20:23 -0300

    Rudimentary way to control the matplotlib animation

--- playground.py
+++ playground.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+# import matplotlib.animation as animation
+
+import time
 
 
+arr = np.identity(50)*255
 
 
-arr = np.identity(20)*255
-
-
-plt.imshow(arr, cmap='gray')
-plt.show()
+while True:
+    plt.imshow(arr, cmap='gray')
+    plt.show()
+    # time.sleep(0.5)
+    arr = ((arr==0)*255)
+    plt.cla()
